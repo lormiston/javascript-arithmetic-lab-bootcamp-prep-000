104c0d81d1cf0a23291c4ef60d72344a1dd6c6bd
Lucy Ormiston <lucyormiston@gmail.com>
2018-02-14 08:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-var = parseInt(a, b)
+var a, b
 
 function add() {
+  parseInt(a, b)
   return a += b
 }
 
 function subtract() {
+  parseInt(a, b)
   return a -= b
 } 
 
 function multiply() {
+  parseInt(a, b)
   return a *= b
 } 
 
 function divide() {
+ parseInt(a, b)
  return a /= b
 }
 
 function inc(){
+  parseInt(a, b)
   return n++
 }
 
 function dec(){
+  parseInt(a, b)
   return n--
 }
 
